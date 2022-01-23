4cd767e04740867bfee0b0068537c9c505c67010
ian038 <ianphua3@gmail.com>
2022-01-23 16:22:46 -0500

    Removed unnecessary dependencies

---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify Clone</title>
+      </Head>
+      <h1>Hello world</h1>
+
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+    </div>
+  )
+}
