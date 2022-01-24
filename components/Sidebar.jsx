11d10298b853441b23ca9287098930ba54670864
ian038 <ianphua3@gmail.com>
2022-01-23 22:26:09 -0500

    Added session provider

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon } from '@heroicons/react/outline'
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function Sidebar() {
+    const { data: session, status } = useSession()
+
+    console.log(session)
+
     return (
         <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
             <div className='space-y-4'>
                 <button onClick={() => signOut()} className='flex items-center space-x-2 hover:text-white'>
-                    <HomeIcon className='h-5 w-5' />
                     <p>Logout</p>
                 </button>
                 <button className='flex items-center space-x-2 hover:text-white'>
