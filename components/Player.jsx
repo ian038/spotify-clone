5f2b8dd2a572352115580596bf89f6605e0e7f52
ian038 <ianphua3@gmail.com>
2022-01-25 18:49:00 -0500

    Added volume adjustment functionality

--- components/Player.jsx
+++ components/Player.jsx
@@ -1,7 +1,8 @@
 import { SwitchHorizontalIcon } from '@heroicons/react/outline';
-import { FastForwardIcon, PauseIcon, ReplyIcon, RewindIcon, PlayIcon, VolumeUpIcon } from '@heroicons/react/solid';
+import { FastForwardIcon, PauseIcon, ReplyIcon, RewindIcon, PlayIcon, VolumeUpIcon, MinusCircleIcon } from '@heroicons/react/solid';
+import { debounce } from 'lodash';
 import { useSession } from 'next-auth/react';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 import useSongInfo from '../hooks/useSongInfo';
@@ -9,7 +10,7 @@ import useSpotify from '../hooks/useSpotify';
 
 export default function Player() {
     const spotifyApi = useSpotify()
-    const { data: session, status } = useSession
+    const { data: session } = useSession()
     const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState)
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
     const [volume, setVolume] = useState(25)
@@ -29,7 +30,6 @@ export default function Player() {
 
     const handlePlayPause = () => {
         spotifyApi.getMyCurrentPlaybackState().then(data => {
-            console.log(data)
             if (data.body.is_playing) {
                 spotifyApi.pause()
                 setIsPlaying(false)
@@ -40,12 +40,25 @@ export default function Player() {
         })
     }
 
+    const debouncedAdjustVolume = useCallback(
+        debounce(volume => {
+            spotifyApi.setVolume(volume).catch(error => { })
+        }, 500),
+        []
+    )
+
     useEffect(() => {
         if (spotifyApi.getAccessToken() && !currentTrackId) {
             fetchCurrentSong()
             setVolume(25)
         }
     }, [currentTrackId, spotifyApi, session])
+
+    useEffect(() => {
+        if (volume > 0 && volume < 100) {
+            debouncedAdjustVolume(volume)
+        }
+    }, [volume])
 
     return (
         <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs 
@@ -70,8 +83,11 @@ export default function Player() {
                 <ReplyIcon className='button' />
             </div>
 
-            <div>
-                <VolumeUpIcon className='button' />
+            <div className='flex items-center space-x-3 md:space-x-4 justify-end pr-5'>
+                <MinusCircleIcon className='button' onClick={() => volume > 0 && setVolume(volume - 10)} />
+                <input onChange={e => setVolume(Number(e.target.value))}
+                    className='w-14 md:w-28' type='range' value={volume} min={0} max={100} />
+                <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className='button' />
             </div>
         </div>
     )
